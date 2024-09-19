66d2a26ef17ed830110d906250381b23f8123f49
rstouten <rstouten@uwyo.edu>
2024-09-19 15:28:09 -0600

    Update Stoutenburg-COSC1010-lab02.py

--- Stoutenburg-COSC1010-lab02.py
+++ Stoutenburg-COSC1010-lab02.py
@@ -23,6 +23,15 @@ date = "1886"
 
 print(f"The {college} was founded in {date}")
 
+# String concatenation 
+    # Finally we will take a look at string concatenation
+    # String concatenation combines two strings together
+    # It is done using the + operator
+    # Create three variables:
+        # first_name, which is your first name 
+        # last_name, which is your last name
+        # space, which is a space character 
+    # Use string concatenation to print out your full name 
 first_name = "reuben"
 last_name = "stoutenburg"
 space = " "
