f4c6887977468d157627098a4ebd2a6b7f7ac2c7
rstouten <rstouten@uwyo.edu>
2024-09-19 15:27:24 -0600

    Update Stoutenburg-COSC1010-lab02.py

--- Stoutenburg-COSC1010-lab02.py
+++ Stoutenburg-COSC1010-lab02.py
@@ -2,6 +2,36 @@
 # UWYO
 # 9/19/2024
 # Lab 02
-# Lab Section:
+# Lab Section: 18
 # Sources
+
+# Print out "Hello, COSC1010"
+print("Hello, COSC1010")
+
+# Assign this string to a variable
+
+hello_message = "Hello, COSC1010"
+print(hello_message)
+
+# create "Cowboy Joe" variable
+
+mascot = "Cowboy Joe"
+print(mascot)
+
+college = "University of Wyoming"
+date = "1886"
+
+print(f"The {college} was founded in {date}")
+
+first_name = "reuben"
+last_name = "stoutenburg"
+space = " "
+full_name = first_name + last_name
+print(full_name.title())
+
+
+
+
+
+
 
